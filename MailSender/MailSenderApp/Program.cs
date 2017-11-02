cf6da5e3d55f05e0e699e67f2272359456ac6ba2
ICT34 <ICT34@DESKTOP-UI9SSS3>
2017-11-02 09:53:22 +0100

    Mail Sender

---
+++ MailSender/MailSenderApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailSender;
+
+namespace MailSenderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailClient mail = new MailClient();
+            mail.SendEmail("luka.faktor@vsmti.hr", "mail", "pozzzzzzzzzz");
+            Console.ReadKey();
+        }
+    }
+}
